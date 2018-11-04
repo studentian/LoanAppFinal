d6b2bb8786838eeff03c301dd6a98c3181de0d0b
Ian Lee <L00150215@student.lyit.ie>
2018-11-04 23:59:33 +0000

    CA2 commit

--- LoanApplication/LoanApplication/Provider.xaml.cs
+++ LoanApplication/LoanApplication/Provider.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -34,5 +35,6 @@ namespace LoanApplication
         {
             stkProviderDetails.Visibility = Visibility.Collapsed;
         }
+
     }
 }
